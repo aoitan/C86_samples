006ea407aaa5e131342283a35ef12460b50d38cc
あおいたん <aoi@nagatsuki-do.net>
2014-07-21 21:16:12 +0900

    add battery status API sample

---
+++ batterystatus/js/battery.js
@@ -0,0 +1,32 @@
+var battery = navigator.battery || navigator.mozBattery;
+var _id = document.getElementById.bind(document);
+
+function levelChange() {
+  var value = _id('level_value');
+  value.innerHTML = battery.level * 100; // parcent
+}
+
+function chargingChange() {
+  var value = _id('charging_value');
+  value.innerHTML = battery.charging;
+}
+
+function chargingTimeChange() {
+  var value = _id('chargingtime_value');
+  value.innerHTML = battery.chargingTime;
+}
+
+function dischargingTimeChange() {
+  var value = _id('dischargingtime_value');
+  value.innerHTML = battery.dischargingTime;
+}
+
+battery.addEventListener("levelchange", levelChange);
+battery.addEventListener("chargingchange", chargingChange);
+battery.addEventListener("chargingtimechange", chargingTimeChange);
+battery.addEventListener("dischargingtimechange", dischargingTimeChange);
+
+levelChange();
+chargingChange();
+chargingTimeChange();
+dischargingTimeChange();
