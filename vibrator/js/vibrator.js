23cc91fdc10bae014c3963b4b30007335cbf07d6
あおいたん <aoi@nagatsuki-do.net>
2014-07-26 23:01:13 +0900

    vibrator

---
+++ vibrator/js/vibrator.js
@@ -0,0 +1,15 @@
+function simpleVibrate() {
+  navigator.vibrate(200);
+}
+
+function threeThreeSevenVibrate() {
+  // 三三七拍子
+  navigator.vibrate([200, 100, 200, 100, 200, 200,
+                     200, 100, 200, 100, 200, 200,
+                     200, 100, 200, 100, 200, 100, 200,
+                     200, 100, 200, 100, 200, 200]);
+}
+
+function stopVibrate() {
+  navigator.vibrate(0);
+}
